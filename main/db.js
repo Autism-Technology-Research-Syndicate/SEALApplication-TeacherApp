9f6b8bcc1933b3fde68fc39672e1e5f01e3379f2
wuruochen <>
2024-10-06 22:42:22 -0700

    merging-with-client-version1.0

--- main/db.js
+++ main/db.js
@@ -2,12 +2,14 @@ const mysql = require('mysql2/promise');
 
 // connect mysql
 const db = mysql.createPool({
-    host: process.env.MSQYL_HOST,     
-    user: process.env.MYSQL_USER,          
-    password: process.env.MYSQL_PASSWORD,  
-    database: process.env.MYSQL_DB,   
-
-
+    //host: process.env.MSQYL_HOST,     
+    //user: process.env.MYSQL_USER,          
+    //password: process.env.MYSQL_PASSWORD,  
+    //database: process.env.MYSQL_DB,   
+    host: "localhost",
+    user: "root",
+    password: "Wukong0987#",
+    database: "test_schema"
 
   });
 // export db
